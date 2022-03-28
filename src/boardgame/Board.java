08402119f46031f7dd7320fb04a15fb7bd225ad6
Aggeo Ribeiro <aggeodossantos@gmail.com>
2022-03-28 20:33:18 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -35,6 +35,18 @@ public class Board {
 		}
 		return pieces [position.getRow()][position.getColumn()];
 	}
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException ("Position not on the board");
+		}
+		if(piece(position)==null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position=null;
+		pieces[position.getRow()][position.getColumn()]=null;
+		return aux;
+	}
 	
 	public void placePiece (Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
@@ -58,6 +70,7 @@ public class Board {
 		}
 		return piece(position) !=null;
 	}
+	
 	
 	
 }
